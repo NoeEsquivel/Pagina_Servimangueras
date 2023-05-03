8929d377bd4f846783cf886314a2871903de73d3
NoeEsquivel <eesquivelc@miumg.edu.gt>
2023-05-03 17:38:11 -0600

    primer commit de prueba

---
+++ web/WEB-INF/newjavascript.js
@@ -0,0 +1,8 @@
+function validateForm() {
+  var nombre = document.forms["myForm"]["nombre"].value;
+  var apellido = document.forms["myForm"]["apellido"].value;
+  var edad = document.forms["myForm"]["edad"].value;
+  var email = document.forms["myForm"]["email"].value;
+  var contrasena = document.forms["myForm"]["contrasena"].value;
+  
+}
